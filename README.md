10771d224c60909e8d72abe7414e3df81374fca7
Victor Brugnolo <brugnolovictor@gmail.com>
2025-11-04 15:23:29 -0300

    add prettier to project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 A implementation of https://www.tabnews.com.br/ for https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Dude, Welcome to TabNews Clone!</h1>;
+  return <h1>Dude, Welcome to TabNews Clone!</h1>;
 }
 
 export default Home;
